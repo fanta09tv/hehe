a20a2df7265d805bedb50e07bfce7f47bebbdbfc
fanta09tv <75021036+fanta09tv@users.noreply.github.com>
2023-08-30 21:41:02 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export const metadata = {
   title: '발로봇 - 한국인들의 발로란트 디스코드 봇',
   description: '발로봇을 즐겨보세요!',
   icons: 'https://media.discordapp.net/attachments/1045603394271838218/1135739430779490345/image.png',
+  verification: {
+    google: "4bT2sBG2vI_lOb4XB8wBdGs1FcZagZfCR3OyeFV3y6M",
+  },
 }
 
 export default function RootLayout({
