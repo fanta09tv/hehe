7d4c37df4456582eb78d4259febf8cc482bed648
fanta09tv <75021036+fanta09tv@users.noreply.github.com>
2024-08-09 09:00:49 +0900

    Add files via upload

--- app/(auth)/acknowledgements/page.tsx
+++ app/(auth)/acknowledgements/page.tsx
@@ -22,11 +22,16 @@ export default function discord() {
             <h5 className="h5">인게임 오브젝트들과 경쟁 전적과 프로필 -{">"} https://valorant-api.com/</h5>
             <h4 className="h4">ﾠﾠ</h4>
             <h4 className="h4">// doubleslash</h4>
-            <h5 className="h5">봇 인증 노하우와 봇 피드백 -{">"} @22_by</h5>
+            <h5 className="h5">봇 인증 노하우와 봇 피드백 -{">"} @ln_.nl</h5>
             <h4 className="h4">ﾠﾠ</h4>
-            <h4 className="h4">LIGHTBLACK</h4>
-            <h5 className="h5">좀 모자라긴 해도 초창기 홍보 -{">"} @light_black1</h5>
+            <h4 className="h4">노루</h4>
+            <h5 className="h5">발로봇에 노루 없으면 안굴러가요 -{">"} @norhu1130</h5>
             <h4 className="h4">ﾠﾠ</h4>
+            <h4 className="h4">소현</h4>
+            <h5 className="h5">봇 프로필 제작, 초창기 팀디팔 -{">"} @sohyune</h5>
+            <h4 className="h4">ﾠﾠ</h4>
+            <h4 className="h4">그외 모든 분들</h4>
+            <h5 className="h5">여러분 덕분에 발로봇이 지금 자리까지 올 수 있었어요. -{">"} @everyone</h5>
           </div>
 
           {/* Form */}
--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const inter = Inter({
 export const metadata = {
   title: '발로봇 - 한국인들의 발로란트 디스코드 봇',
   description: '발로봇을 즐겨보세요!',
-  icons: 'https://media.discordapp.net/attachments/1045603394271838218/1135739430779490345/image.png',
+  icons: 'https://media.discordapp.net/attachments/1270936951893262376/1270936967793868864/val_logo.png?ex=66b583c9&is=66b43249&hm=22674582aa3662452cff8f31d678ddb79cf53eac83c300b8aa1f26518c2478d2&=&format=webp&quality=lossless&width=453&height=437',
   verification: {
     google: "4bT2sBG2vI_lOb4XB8wBdGs1FcZagZfCR3OyeFV3y6M",
   },
