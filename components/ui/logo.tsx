4f5853a8c9f4cdebd2c2ba546c8b9ee04cb47e81
fanta09tv <75021036+fanta09tv@users.noreply.github.com>
2024-09-28 23:11:20 +0900

    Update logo.tsx

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="Cruip">
           <picture id="footer-logo" >
-            <img src="https://media.discordapp.net/attachments/1151062950539427861/1283344911332151346/a.png?ex=66e2a796&is=66e15616&hm=c3f64e9269649fc723f8cfce951b093c46ad06f1e59cc60e8a71b7ad416b0e4a&=&format=webp&quality=lossless&width=245&height=91" alt="Logo"></img>
+            <img src="https://media.discordapp.net/attachments/1151062950539427861/1283344911332151346/a.png?ex=66f91116&is=66f7bf96&hm=71ce1d9823c809b2c9c9fdae07f2a92d977efa8d2bdd5900df45b4aba08d42b6&=&format=webp&quality=lossless&width=245&height=91" alt="Logo"></img>
           </picture>
         <rect width="32" height="32" rx="16" fill="url(#footer-logo)" fillRule="nonzero"/>
     </Link>
