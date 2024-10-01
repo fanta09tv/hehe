d41140872ad470004218f58165b9f3a34de55fab
fanta09tv <75021036+fanta09tv@users.noreply.github.com>
2024-10-01 11:20:03 +0900

    Update logo.tsx

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import LogoImage from '@/public/images/lol.png'
 
 export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="Cruip">
           <picture id="footer-logo" >
-            <img src="https://media.discordapp.net/attachments/1151062950539427861/1283344911332151346/a.png?ex=66f91116&is=66f7bf96&hm=71ce1d9823c809b2c9c9fdae07f2a92d977efa8d2bdd5900df45b4aba08d42b6&=&format=webp&quality=lossless&width=245&height=91" alt="Logo"></img>
+             <Image src={LogoImage} width={245} height={91} alt="Banner" />
           </picture>
         <rect width="32" height="32" rx="16" fill="url(#footer-logo)" fillRule="nonzero"/>
     </Link>
