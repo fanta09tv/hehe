6bb883b0e4bc4bab099f0cd676ab43ea55952d5c
fanta09tv <75021036+fanta09tv@users.noreply.github.com>
2024-10-01 11:46:12 +0900

    Update logo.tsx

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -6,7 +6,7 @@ export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="Cruip">
           <picture id="footer-logo" >
-             <Image src={LogoImage} width={245} height={91} alt="Banner" />
+             <Image src={LogoImage} width={130} height={48} alt="Banner" />
           </picture>
         <rect width="32" height="32" rx="16" fill="url(#footer-logo)" fillRule="nonzero"/>
     </Link>
