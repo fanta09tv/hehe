91a465331158ce358ce36fc06d24dfafd308f409
fanta09tv <75021036+fanta09tv@users.noreply.github.com>
2024-10-01 11:23:06 +0900

    Update logo.tsx

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import LogoImage from '@/public/images/lol.png'
+import LogoImage from '@/public/images/a.png'
 
 export default function Logo() {
   return (
