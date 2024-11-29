51e75bb59723634641b895da1cf0ab8430361d69
fanta09tv <75021036+fanta09tv@users.noreply.github.com>
2024-11-29 22:39:53 +0900

    page.tsx 업데이트

--- app/(auth)/invite/page.tsx
+++ app/(auth)/invite/page.tsx
@@ -23,7 +23,7 @@ export default function discord() {
             <h3 className="h3">3초후 리다이렉트 됩니다.</h3>
             <h2 className="h2">ﾠﾠ</h2>
             <div>
-              <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://discord.com/oauth2/authorize?client_id=1040206071417012234&permissions=8&scope=bot">리다이렉트 되지 않을시 클릭하세요</a>
+              <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://discord.com/oauth2/authorize?client_id=1299774023822999612&permissions=8&scope=bot">리다이렉트 되지 않을시 클릭하세요</a>
             </div>
           </div>
 
